5b69fbdc4ab2416112f18f436d11f3b575a0c483
eborisov <eborisov@sinergo.ru>
2022-05-23 02:54:36 +0700

    add clustering for sf_factor computing in baseline

--- src/service/luhn_summarizer.py
+++ src/service/luhn_summarizer.py
@@ -3,6 +3,7 @@ import pandas as pd
 from razdel import sentenize
 from nltk.corpus import stopwords
 from text_utils import tokenize
+import numpy as np
 # download stopwords corpus, you need to run it once
 # nltk.download("stopwords")
 
@@ -13,10 +14,13 @@ from pymystem3 import Mystem
 mystem = Mystem()
 russian_stopwords = stopwords.words("russian")
 
+
 class LuhnSummarizer():
     def __init__(self, is_lemmatize: bool = True):
         self.is_lemmatize = True
-        self.sf_word_threshold = 0.25
+        self.sf_word_threshold = 0.002
+        self.sf_sentence_threshold = 0.3
+        self.max_not_sf_seq_words = 4
 
     def custom_sentenize(self, text: str) -> list:
         sents = []
@@ -54,31 +58,50 @@ class LuhnSummarizer():
         # log(f"tokens:{tokens}")
 
         tokens = tokenize(text)
-        vectorizer = TfidfVectorizer()  # (use_idf=False)
+        vectorizer = TfidfVectorizer(use_idf=False)
         X = vectorizer.fit_transform(tokens)
         features_names_out = vectorizer.get_feature_names_out()
 
         freq_df = pd.DataFrame(X.toarray(), columns=features_names_out)  # составляем датафрейм частотностей слов
-        freq_dict = {word: np.sum(freq_df[word].values) for word in features_names_out}
+        # print(f"Features Names Out: {features_names_out}")
+
+        freq_dict = {word: np.sum(freq_df[word].values) / len(list(freq_df.keys())) for word in features_names_out}
+
+        freq_dict = dict(sorted(freq_dict.items(), key=lambda y: y[1],
+                                reverse=True))  # сортируем словарь частотностей слов по убыванию
+        # print(f"Freq Dict before split by threshold: {freq_dict}")
+        freq_dict = {k: v for k, v in freq_dict.items() if v >= self.sf_word_threshold}
 
         return freq_dict
 
+    def get_sentence_significance_word_mask(self, sentence_words_mask: list) -> list:
+        first_sf_word_indx = sentence_words_mask.index(1)
+        last_sf_word_indx = len(sentence_words_mask) - 1 - sentence_words_mask[::-1].index(1)
+
+        return sentence_words_mask[first_sf_word_indx: last_sf_word_indx + 1]
+
     def compute_significance_factor(self, freq_dict: dict, sentence: list) -> np.float16:
+        sentence_words_mask = [1 if freq_dict[word] else 0 \
+                               for word in sentence if word in freq_dict.keys()]
+        # example of sentence_words_mask: [0, 0, 1, 0, 1, 1, 0, 0, 1, 0, 0] 1 - sf word in sentence and 0 - not sf word
+        # print(sentence_words_mask)
 
-        all_words_count = len(sentence)
-        number_of_sf_words = sum(1 if freq_dict[word] / all_words_count > self.sf_word_threshold else 0 \
-                                 for word in sentence if word in freq_dict.keys())
+        if sum(sentence_words_mask) == 0:
+            return 0
 
-        significance_factor = number_of_sf_words ** 2 / all_words_count
+        sentence_words_mask = self.get_sentence_significance_word_mask(sentence_words_mask)
+        number_of_sf_words = sum(sentence_words_mask)
+        total_number_of_bracketed_words = len(sentence_words_mask)
+
+        # print(f"number_of_sf_words: {number_of_sf_words}, total_number_of_bracketed_words: {total_number_of_bracketed_words}")
+        significance_factor = number_of_sf_words ** 2 / total_number_of_bracketed_words
 
         return significance_factor
-
-    def get_sentence_significance_word_cluster(self, freq_dict: dict, text: str) -> str:
-        pass
 
     def summarize(self, text: str) -> str:
         # sentence segmentation
         sentences = self.custom_sentenize(text)
+        # print(len(sentences))
         # log(sentences)
 
         text_freq_dict = self.create_word_freq_dict(text)
@@ -95,10 +118,12 @@ class LuhnSummarizer():
 
         sentences_sf.sort(reverse=True)
 
-        ranking_ind_uppper_bound = int(len(sentences_sf) * 0.05) if len(sentences_sf) * 0.05 > 1 else 1
+        sentence_sf_threshold_percentile_75 = np.percentile(sentences_sf, 75)
+        # print(sentences_sf)
+        # print(sentences_sf[ranking_ind_uppper_bound])
         # log(min(sentences_sf[:ranking_ind_uppper_bound]))
 
         summary_sentences = [sent for i, sent in enumerate(sentences) if
-                             sentences_sf[i] >= sentences_sf[ranking_ind_uppper_bound]]
+                             sentences_sf[i] >= sentence_sf_threshold_percentile_75]
 
-        return summary_sentences
+        return "".join(summary_sentences)
